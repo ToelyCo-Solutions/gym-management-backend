4eefd53f8331e824c1a67665880abc95a60ee9ad
venkadesanmurugesan <venkadesanmurugesan2209@gmail.com>
2023-03-26 00:28:14 +0530

    base setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+require("dotenv").config();
+
+function corsMiddleware(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    corsMiddleware(req, res, () => {
+      res.end("i am backend");
+    });
+  } else if (req.url === "/api" && req.method === "GET") {
+    corsMiddleware(req, res, () => {
+      const data = { message: "Hello, world!" };
+      const jsonData = JSON.stringify(data);
+
+      res.setHeader("Content-Type", "application/json");
+      res.end(jsonData);
+    });
+  } else {
+    res.end();
+  }
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("Server running at http://127.0.0.1:5055");
+});
